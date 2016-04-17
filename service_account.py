d0ae3317e9bc4900f98e0d490ff20e50df2ae1e5
Clo <Clo@Clos-MacBook-Pro.local>
2016-04-17 21:10:52 +0100

    First Commit of Pebble analytics Dashboard

---
+++ service_account.py
@@ -0,0 +1,102 @@
+# service-account.py
+
+import json
+from oauth2client.client import SignedJwtAssertionCredentials
+
+# The scope for the OAuth2 request.
+SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+
+# The location of the key file with the key data.
+KEY_FILEPATH = 'path/to/json-key.json'
+
+# Load the key file's private data.
+with open(KEY_FILEPATH) as key_file:
+  _key_data = json.load(key_file)
+
+# Construct a credentials objects from the key data and OAuth2 scope.
+_credentials = SignedJwtAssertionCredentials(
+    _key_data['client_email'], _key_data['private_key'], SCOPE)
+
+# Defines a method to get an access token from the credentials object.
+# The access token is automatically refreshed if it has expired.
+def get_access_token():
+  return _credentials.get_access_token().access_token
+
+  <script>
+(function(w,d,s,g,js,fs){
+  g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
+  js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
+  js.src='https://apis.google.com/js/platform.js';
+  fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
+}(window,document,'script'));
+</script>
+
+<div id="chart-1-container"></div>
+<div id="chart-2-container"></div>
+
+<script>
+
+gapi.analytics.ready(function() {
+
+  /**
+   * Authorize the user with an access token obtained server side.
+   */
+  gapi.analytics.auth.authorize({
+    'serverAuth': {
+      'access_token': '{{ ACCESS_TOKEN_FROM_SERVICE_ACCOUNT }}'
+    }
+  });
+
+
+  /**
+   * Creates a new DataChart instance showing sessions over the past 30 days.
+   * It will be rendered inside an element with the id "chart-1-container".
+   */
+  var dataChart1 = new gapi.analytics.googleCharts.DataChart({
+    query: {
+      'ids': 'ga:100367422', // The Demos & Tools website view.
+      'start-date': '30daysAgo',
+      'end-date': 'yesterday',
+      'metrics': 'ga:sessions,ga:users',
+      'dimensions': 'ga:date'
+    },
+    chart: {
+      'container': 'chart-1-container',
+      'type': 'LINE',
+      'options': {
+        'width': '100%'
+      }
+    }
+  });
+  dataChart1.execute();
+
+
+  /**
+   * Creates a new DataChart instance showing top 5 most popular demos/tools
+   * amongst returning users only.
+   * It will be rendered inside an element with the id "chart-3-container".
+   */
+  var dataChart2 = new gapi.analytics.googleCharts.DataChart({
+    query: {
+      'ids': 'ga:100367422', // The Demos & Tools website view.
+      'start-date': '30daysAgo',
+      'end-date': 'yesterday',
+      'metrics': 'ga:pageviews',
+      'dimensions': 'ga:pagePathLevel1',
+      'sort': '-ga:pageviews',
+      'filters': 'ga:pagePathLevel1!=/',
+      'max-results': 7
+    },
+    chart: {
+      'container': 'chart-2-container',
+      'type': 'PIE',
+      'options': {
+        'width': '100%',
+        'pieHole': 4/9,
+      }
+    }
+  });
+  dataChart2.execute();
+
+});
+</script>
